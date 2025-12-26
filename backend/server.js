2bcb0e839a272f05f054423492b1d3223d3c1df4
jeewanlamsal <106655205+jeewanlamsal@users.noreply.github.com>
2025-12-26 09:00:42 +0545

    Update CORS middleware to specify origins and methods

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors()); // Allows your frontend to talk to this backend
+app.use(cors({
+    origin: ["http://localhost:5173", "https://mern-to-do-3zzw.vercel.app"], 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+})); // Allows your frontend to talk to this backend
 app.use(express.json()); // Parses incoming JSON requests
 
 // Route Links
